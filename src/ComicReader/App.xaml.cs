c3d4b7bdb7e2a252e2e7d03ff8504b69b2cd91f9
aicd0 <45353590+aicd0@users.noreply.github.com>
2025-08-06 22:15:38 +0800

    File activation for portable app (#99)
    * Portable app support file activation
    
    * Fix file activation

--- src/ComicReader/App.xaml.cs
+++ src/ComicReader/App.xaml.cs
@@ -2,10 +2,15 @@
 // Licensed under the MIT License.
 
 using System;
+using System.IO;
+using System.Runtime.InteropServices;
 
 using ComicReader.Common;
 using ComicReader.Common.InitTask;
+using ComicReader.SDK.Common.AppEnvironment;
 using ComicReader.SDK.Common.DebugTools;
+using ComicReader.SDK.Common.Native;
+using ComicReader.SDK.Common.Storage;
 
 using Microsoft.UI.Xaml;
 using Microsoft.Windows.AppLifecycle;
@@ -17,6 +22,7 @@ namespace ComicReader;
 public partial class App : Application
 {
     private const string TAG = nameof(App);
+    private const string COMMAND_LINE_FILE_NAME = "command_line.txt";
 
     internal static readonly WindowManager<MainWindow> WindowManager = new();
 
@@ -42,6 +48,12 @@ public partial class App : Application
         // isn't the "main" instance.
         if (!mainInstance.IsCurrent)
         {
+            // If the app is running in portable mode, store the command line arguments
+            if (EnvironmentProvider.IsPortable())
+            {
+                StoreCommandLine();
+            }
+
             // Redirect the activation (and args) to the "main" instance, and exit.
             await mainInstance.RedirectActivationToAsync(activatedEventArgs);
             System.Diagnostics.Process.GetCurrentProcess().Kill();
@@ -49,27 +61,157 @@ public partial class App : Application
         }
 
         _initTaskManager.InitOnAppLaunch();
-
-        // Initialize MainWindow here
-        MainWindow.Open(recoverTabs: true);
-
         mainInstance.Activated += OnActivated;
-        OnActivated(null, activatedEventArgs);
+        OnActivatedInternal(activatedEventArgs, firstLaunch: true);
     }
 
     private void OnActivated(object? sender, AppActivationArguments e)
     {
-        if (e.Kind == ExtendedActivationKind.File)
+        OnActivatedInternal(e, firstLaunch: false);
+    }
+
+    private void OnActivatedInternal(AppActivationArguments e, bool firstLaunch)
+    {
+        string[] cmdArgs;
+        if (EnvironmentProvider.IsPortable())
         {
-            MainWindow? window = WindowManager.GetAnyWindow();
-            if (window != null)
+            string? commandLine;
+            if (firstLaunch)
             {
-                window.OnFileActivated((FileActivatedEventArgs)e.Data);
+                commandLine = Environment.CommandLine;
             }
             else
             {
-                Logger.F(TAG, "Failed to perform file activation, no window is found.");
+                commandLine = TryReadCommandLine();
+                if (string.IsNullOrEmpty(commandLine))
+                {
+                    return;
+                }
             }
+
+            string[] splitedCommandLine = SplitCommandLine(commandLine);
+            if (splitedCommandLine.Length >= 1)
+            {
+                cmdArgs = splitedCommandLine[1..];
+            }
+            else
+            {
+                cmdArgs = splitedCommandLine;
+            }
+        }
+        else
+        {
+            switch (e.Kind)
+            {
+                case ExtendedActivationKind.File:
+                    {
+                        var fileArgs = (FileActivatedEventArgs)e.Data;
+                        cmdArgs = [fileArgs.Files[0].Path];
+                    }
+                    break;
+                default:
+                    cmdArgs = [];
+                    break;
+            }
+        }
+
+        string cmd = string.Join(' ', cmdArgs);
+        Logger.I(TAG, $"OnActivated: firstLaunch={firstLaunch}, cmd={cmd}");
+
+        if (firstLaunch)
+        {
+            MainWindow.Open(cmdArgs);
+        }
+        else
+        {
+            MainWindow? window = WindowManager.GetAnyWindow();
+            if (window is null)
+            {
+                Logger.F(TAG, "Failed to perform file activation, no window is found.");
+                return;
+            }
+
+            window.OnCommandLine(cmdArgs);
+        }
+    }
+
+    private static void StoreCommandLine()
+    {
+        string commandLine = Environment.CommandLine;
+        string temporaryFolderPath = StorageLocation.TemporaryFolderPath;
+        if (!Directory.Exists(temporaryFolderPath))
+        {
+            Directory.CreateDirectory(temporaryFolderPath);
+        }
+
+        string commandLineFile = Path.Combine(temporaryFolderPath, COMMAND_LINE_FILE_NAME);
+        File.WriteAllText(commandLineFile, commandLine);
+    }
+
+    private static string? TryReadCommandLine()
+    {
+        string temporaryFolderPath = StorageLocation.TemporaryFolderPath;
+        if (!Directory.Exists(temporaryFolderPath))
+        {
+            return null;
+        }
+
+        string commandLineFile = Path.Combine(temporaryFolderPath, COMMAND_LINE_FILE_NAME);
+        if (!File.Exists(commandLineFile))
+        {
+            return null;
+        }
+
+        string content;
+        try
+        {
+            content = File.ReadAllText(commandLineFile);
+        }
+        catch (Exception e)
+        {
+            Logger.F(TAG, nameof(TryReadCommandLine), e);
+            return null;
+        }
+
+        try
+        {
+            File.Delete(commandLineFile);
+        }
+        catch (Exception e)
+        {
+            Logger.F(TAG, nameof(TryReadCommandLine), e);
+        }
+
+        return content;
+    }
+
+    private static string[] SplitCommandLine(string commandLine)
+    {
+        if (string.IsNullOrEmpty(commandLine))
+        {
+            return [];
+        }
+
+        IntPtr argv = NativeMethods.CommandLineToArgvW(commandLine, out int argc);
+        if (argv == IntPtr.Zero)
+        {
+            return [];
+        }
+
+        try
+        {
+            string[] args = new string[argc];
+            for (int i = 0; i < argc; i++)
+            {
+                IntPtr p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
+                args[i] = Marshal.PtrToStringUni(p)!;
+            }
+
+            return args;
+        }
+        finally
+        {
+            NativeMethods.LocalFree(argv);
         }
     }
 }
