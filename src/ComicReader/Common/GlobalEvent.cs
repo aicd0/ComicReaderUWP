6a3e70821481ebf2bc74e584881f74ce3991d4a7
aicd0 <2567150489@qq.com>
2025-08-30 13:01:10 +0800

    Delay global events

--- src/ComicReader/Common/GlobalEvent.cs
+++ src/ComicReader/Common/GlobalEvent.cs
@@ -16,10 +16,10 @@ internal class GlobalEvent
 
     private GlobalEvent() { }
 
-    public IMutableLiveData<object> ComicUpdated = new MutableLiveDataWithMinInterval<object>(EventBus.Default.With("ComicUpdated"), 1000);
-    public IMutableLiveData<object> FavoriteUpdated = new MutableLiveDataWithMinInterval<object>(EventBus.Default.With("FavoriteUpdated"), 1000);
-    public IMutableLiveData<object> HistoryUpdated = new MutableLiveDataWithMinInterval<object>(EventBus.Default.With("HistoryUpdated"), 1000);
-    public IMutableLiveData<object> TagInfoUpdated = new MutableLiveDataWithMinInterval<object>(EventBus.Default.With("TagInfoUpdated"), 1000);
+    public IMutableLiveData<object> ComicUpdated = new MutableLiveDataWithMinInterval<object>(EventBus.Default.With("ComicUpdated"), 1000, delay: 100);
+    public IMutableLiveData<object> FavoriteUpdated = new MutableLiveDataWithMinInterval<object>(EventBus.Default.With("FavoriteUpdated"), 1000, delay: 100);
+    public IMutableLiveData<object> HistoryUpdated = new MutableLiveDataWithMinInterval<object>(EventBus.Default.With("HistoryUpdated"), 1000, delay: 100);
+    public IMutableLiveData<object> TagInfoUpdated = new MutableLiveDataWithMinInterval<object>(EventBus.Default.With("TagInfoUpdated"), 1000, delay: 100);
     public IMutableLiveData<object> HotKeyF10 = EventBus.Default.With("HotKeyF10");
     public IMutableLiveData<object> HotKeyF11 = EventBus.Default.With("HotKeyF11");
 }
