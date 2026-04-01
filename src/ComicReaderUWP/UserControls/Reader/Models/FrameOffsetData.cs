7a7a2bc077b05858ec904d3342c06460d6cdab48
aicd0 <45353590+aicd0@users.noreply.github.com>
2026-04-01 23:43:56 +0800

    Rename

--- src/ComicReaderUWP/UserControls/Reader/Models/FrameOffsetData.cs
+++ src/ComicReaderUWP/UserControls/Reader/Models/FrameOffsetData.cs
@@ -1,7 +1,7 @@
 // Copyright (c) aicd0. All rights reserved.
 // Licensed under the MIT License.
 
-namespace ComicReaderUWP.UserControls.Reader;
+namespace ComicReaderUWP.UserControls.Reader.Models;
 
 internal struct FrameOffsetData
 {
