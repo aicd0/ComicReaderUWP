0234c831d47f94a0ef255623c7932e04787e91e5
aicd0 <45353590+aicd0@users.noreply.github.com>
2026-06-21 21:44:13 +0800

    Refactor tests

--- src/ComicReaderUWP/UserControls/Reader/PageLayout/SimplePageLayoutManager.cs
+++ src/ComicReaderUWP/UserControls/Reader/PageLayout/SimplePageLayoutManager.cs
@@ -2,9 +2,9 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
+using ComicReaderUWP.Core.Test;
 using ComicReaderUWP.UserControls.Reader.Imaging;
 
 namespace ComicReaderUWP.UserControls.Reader.PageLayout;
@@ -226,75 +226,5 @@ internal class SimplePageLayoutManager : IPageLayoutManager
         public int Width { get; init; }
         public int Height { get; init; }
         public PageLayoutInfo? Layout { get; set; }
-    }
-}
-
-public static class SpreadDetectionHelper
-{
-    public class PageSamples
-    {
-        private const float MIN_ASPECT_RATIO = 0.6F;
-
-        private readonly SortedDictionary<float, int> _aspectRatios = [];
-        private int _aspectRatiosCount = 0;
-
-        public int Count => _aspectRatiosCount;
-
-        public void Clear()
-        {
-            _aspectRatiosCount = 0;
-            _aspectRatios.Clear();
-        }
-
-        public void Add(float aspectRatio)
-        {
-            _aspectRatiosCount++;
-            if (_aspectRatios.TryGetValue(aspectRatio, out int count))
-            {
-                _aspectRatios[aspectRatio] = count + 1;
-            }
-            else
-            {
-                _aspectRatios[aspectRatio] = 1;
-            }
-        }
-
-        public bool IsSpreadPage(float aspectRatio)
-        {
-            if (Count <= 2)
-            {
-                return false;
-            }
-
-            if (aspectRatio < MIN_ASPECT_RATIO)
-            {
-                // Too narrow to be a spread page
-                return false;
-            }
-
-            float previous = -1F;
-            float threshold = -1F;
-            foreach (KeyValuePair<float, int> kvp in _aspectRatios)
-            {
-                float current = kvp.Key;
-
-                if (previous >= MIN_ASPECT_RATIO &&
-                    current >= MIN_ASPECT_RATIO &&
-                    current / previous >= 1.5F)
-                {
-                    threshold = (previous + current) * 0.5F;
-                    break;
-                }
-
-                previous = current;
-            }
-
-            if (threshold < 0)
-            {
-                return false;
-            }
-
-            return aspectRatio > threshold;
-        }
     }
 }
