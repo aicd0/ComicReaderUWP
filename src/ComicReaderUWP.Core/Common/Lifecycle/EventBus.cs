203bbd63b8b90e337fcdcc29b1df85ba2e1a8d04
aicd0 <45353590+aicd0@users.noreply.github.com>
2026-08-20 16:21:15 +0900

    Fix event bus crash

--- src/ComicReaderUWP.Core/Common/Lifecycle/EventBus.cs
+++ src/ComicReaderUWP.Core/Common/Lifecycle/EventBus.cs
@@ -1,30 +1,31 @@
 // Copyright (c) aicd0. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections.Concurrent;
+
 namespace ComicReaderUWP.Core.Common.Lifecycle;
 
 public class EventBus : IEventBus
 {
     public static readonly IEventBus Default = new EventBus();
 
-    private readonly Dictionary<string, ILiveDataCommonAbility> _topics = [];
-    private bool _clearing = false;
+    private readonly ConcurrentDictionary<string, ILiveDataCommonAbility> _topics = [];
 
     public IMutableLiveData<T> With<T>(string eventId) where T : notnull
     {
-        if (_clearing)
+        while (true)
         {
-            return new EmptyLiveData<T>();
-        }
+            if (_topics.TryGetValue(eventId, out ILiveDataCommonAbility? topic))
+            {
+                return (IMutableLiveData<T>)topic;
+            }
 
-        if (_topics.TryGetValue(eventId, out ILiveDataCommonAbility? topic))
-        {
-            return (IMutableLiveData<T>)topic;
+            var newTopic = new MutableLiveData<T>();
+            if (_topics.TryAdd(eventId, newTopic))
+            {
+                return newTopic;
+            }
         }
-
-        var newTopic = new MutableLiveData<T>();
-        _topics.Add(eventId, newTopic);
-        return newTopic;
     }
 
     public IMutableLiveData<object> With(string eventId)
@@ -34,19 +35,16 @@ public class EventBus : IEventBus
 
     public void Clear()
     {
-        _clearing = true;
-        try
+        while (!_topics.IsEmpty)
         {
-            foreach (ILiveDataCommonAbility topic in _topics.Values)
+            List<string> keys = [.. _topics.Keys];
+            foreach (string key in keys)
             {
-                topic.Clear();
+                if (_topics.TryRemove(key, out ILiveDataCommonAbility? topic))
+                {
+                    topic.Clear();
+                }
             }
-
-            _topics.Clear();
-        }
-        finally
-        {
-            _clearing = false;
         }
     }
 }
